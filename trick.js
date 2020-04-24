cd16870072258469bc7470281f00fa198de90ac1
lohith451 <62667913+lohith451@users.noreply.github.com>
2020-04-24 14:43:56 -0500

    Add files via upload

--- trick.js
+++ trick.js
@@ -5,22 +5,23 @@ var offsetX=c.offsetLeft;
 var offsetY=c.offsetTop;
 var canvasHeight = c.getAttribute('height');
 var canvasWidth = c.getAttribute('width');
-var score = 0;
 var color1 = "GREY";
 var color2 = "transparent";
 var color3 = "green";
-var x1 = canvasHeight/14;
-var y1 = canvasWidth/2;
-var x2 = canvasHeight/3.5;
-var y2 = canvasWidth/1.4;
-var x3 = canvasHeight/1.4;
-var y3 = canvasWidth/1.4;
-var x4 = canvasHeight/1.07692308;
-var y4 = canvasWidth/2;
-var x5 = canvasHeight/1.4;
-var y5 = canvasWidth/3.5;
-var x6 = canvasHeight/3.5;
-var y6 = canvasWidth/3.5;
+var color4 = "red";
+var x1 = canvasWidth/1.4;
+var y1 = canvasHeight/7.77777778;
+var x2 = canvasWidth/3.5;
+var y2 = canvasHeight/7.77777778;
+var x3 = canvasWidth/14;
+var y3 = canvasHeight/2;
+var x4 = canvasWidth/3.5;
+var y4 = canvasHeight/1.14754098;
+var x5 = canvasWidth/1.4;
+var y5 = canvasHeight/1.14754098;
+var x6 = canvasWidth/1.07692308;
+var y6 = canvasHeight/2;
+var sec = 250;
 var lineRect1=defineLineAsRect(x1,y1,x2,y2,4);
 drawLine(x1,y1,x2,y2,3,color1);
 var lineRect2=defineLineAsRect(x2,y2,x3,y3,4);
@@ -32,7 +33,7 @@ drawLine(x4,y4,x5,y5,3,color1);
 var lineRect5=defineLineAsRect(x5,y5,x6,y6,4);
 drawLine(x5,y5,x6,y6,3,color1);
 var lineRect6=defineLineAsRect(x6,y6,x1,y1,4);
-drawLine(x6,x6,x1,y1,3,color1);
+drawLine(x6,y6,x1,y1,3,color1);
 var lineRect7=defineLineAsRect(x1,y1,x4,y4,4);
 drawLine(x1,y1,x4,y4,3,color1);
 var lineRect8=defineLineAsRect(x4,y4,x2,y2,4);
@@ -94,50 +95,188 @@ function defineLineAsRect(x1,y1,x2,y2,lineWidth){
         rectHeight:lineWidth
     });
 }
-function gameOver()  {
-  if((result["Line1"] == 1 && result["Line9"] == 1 && result["Line6"] == 1) ||
-  (result["Line6"] == 1 && result["Line5"] == 1 && result["Line15"] == 1) ||
-  (result["Line7"] == 1 && result["Line4"] == 1 && result["Line15"] == 1) ||
-  (result["Line1"] == 1 && result["Line2"] == 1 && result["Line14"] == 1) ||
-  (result["Line14"] == 1 && result["Line3"] == 1 && result["Line7"] == 1) ||
-  (result["Line14"] == 1 && result["Line15"] == 1 && result["Line11"] == 1) ||
-  (result["Line4"] == 1 && result["Line5"] == 1 && result["Line13"] == 1) ||
-  (result["Line13"] == 1 && result["Line7"] == 1 && result["Line6"] == 1) ||
-  (result["Line2"] == 1 && result["Line3"] == 1 && result["Line8"] == 1) ||
-  (result["Line8"] == 1 && result["Line7"] == 1 && result["Line1"] == 1) ||
-  (result["Line10"] == 1 && result["Line5"] == 1 && result["Line11"] == 1) ||
-  (result["Line10"] == 1 && result["Line9"] == 1 && result["Line2"] == 1) ||
-  (result["Line1"] == 1 && result["Line12"] == 1 && result["Line15"] == 1) ||
-  (result["Line12"] == 1 && result["Line5"] == 1 && result["Line9"] == 1) ||
-  (result["Line12"] == 1 && result["Line11"] == 1 && result["Line2"] == 1) ||
-  (result["Line12"] == 1 && result["Line4"] == 1 && result["Line8"] == 1) ||
-  (result["Line6"] == 1 && result["Line14"] == 1 && result["Line10"] == 1) ||
-  (result["Line13"] == 1 && result["Line9"] == 1 && result["Line8"] == 1) ||
-  (result["Line11"] == 1 && result["Line4"] == 1 && result["Line3"] == 1) ||
-  (result["Line13"] == 1 && result["Line3"] == 1 && result["Line10"] == 1)){
-  alert("Game Over your Score is :"+score+"\n please press ok to restart your game");
-  window.location = "stick.html";
-}else{
-  score++;
-  document.getElementById('score').innerHTML = "<h1>Score : "+score+"</h1>";
+function alert_gameOver(){
+  setTimeout(function(){ alert("Game Over \n please press ok to restart your game");
+  window.location = "stick.html"; }, sec);
 }
+function gameOver(line)  {
+  if((result["Line1"] == 1 && result["Line9"] == 1 && result["Line6"] == 1)){
+    drawLine(x1,y1,x2,y2,3,color4);
+    drawLine(x2,y2,x6,y6,3,color4);
+    drawLine(x6,y6,x1,y1,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line6"] == 1 && result["Line5"] == 1 && result["Line15"] == 1)){
+    drawLine(x6,y6,x1,y1,3,color4);
+    drawLine(x5,y5,x6,y6,3,color4);
+    drawLine(x5,y5,x1,y1,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line7"] == 1 && result["Line4"] == 1 && result["Line15"] == 1)){
+    drawLine(x1,y1,x4,y4,3,color4);
+    drawLine(x4,y4,x5,y5,3,color4);
+    drawLine(x5,y5,x1,y1,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line1"] == 1 && result["Line2"] == 1 && result["Line14"] == 1)){
+    drawLine(x1,y1,x2,y2,3,color4);
+    drawLine(x2,y2,x3,y3,3,color4);
+    drawLine(x1,y1,x3,y3,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line14"] == 1 && result["Line3"] == 1 && result["Line7"] == 1)){
+    drawLine(x1,y1,x3,y3,3,color4);
+    drawLine(x3,y3,x4,y4,3,color4);
+    drawLine(x1,y1,x4,y4,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line14"] == 1 && result["Line15"] == 1 && result["Line11"] == 1)){
+    drawLine(x1,y1,x3,y3,3,color4);
+    drawLine(x5,y5,x1,y1,3,color4);
+    drawLine(x3,y3,x5,y5,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line4"] == 1 && result["Line5"] == 1 && result["Line13"] == 1)){
+    drawLine(x4,y4,x5,y5,3,color4);
+    drawLine(x5,y5,x6,y6,3,color4);
+    drawLine(x4,y4,x6,y6,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line13"] == 1 && result["Line7"] == 1 && result["Line6"] == 1)){
+    drawLine(x4,y4,x6,y6,3,color4);
+    drawLine(x1,y1,x4,y4,3,color4);
+    drawLine(x6,y6,x1,y1,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line2"] == 1 && result["Line3"] == 1 && result["Line8"] == 1)){
+    drawLine(x2,y2,x3,y3,3,color4);
+    drawLine(x3,y3,x4,y4,3,color4);
+    drawLine(x4,y4,x2,y2,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line8"] == 1 && result["Line7"] == 1 && result["Line1"] == 1)){
+    drawLine(x4,y4,x2,y2,3,color4);
+    drawLine(x1,y1,x4,y4,3,color4);
+    drawLine(x1,y1,x2,y2,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line10"] == 1 && result["Line5"] == 1 && result["Line11"] == 1)){
+    drawLine(x6,y6,x3,y3,3,color4);
+    drawLine(x5,y5,x6,y6,3,color4);
+    drawLine(x3,y3,x5,y5,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line10"] == 1 && result["Line9"] == 1 && result["Line2"] == 1)){
+    drawLine(x6,y6,x3,y3,3,color4);
+    drawLine(x2,y2,x6,y6,3,color4);
+    drawLine(x2,y2,x3,y3,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line1"] == 1 && result["Line12"] == 1 && result["Line15"] == 1)){
+    drawLine(x1,y1,x2,y2,3,color4);
+    drawLine(x5,y5,x2,y2,3,color4);
+    drawLine(x5,y5,x1,y1,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line12"] == 1 && result["Line5"] == 1 && result["Line9"] == 1)){
+    drawLine(x5,y5,x2,y2,3,color4);
+    drawLine(x5,y5,x6,y6,3,color4);
+    drawLine(x2,y2,x6,y6,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line12"] == 1 && result["Line11"] == 1 && result["Line2"] == 1)){
+    drawLine(x5,y5,x2,y2,3,color4);
+    drawLine(x3,y3,x5,y5,3,color4);
+    drawLine(x2,y2,x3,y3,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line12"] == 1 && result["Line4"] == 1 && result["Line8"] == 1)){
+    drawLine(x5,y5,x2,y2,3,color4);
+    drawLine(x4,y4,x5,y5,3,color4);
+    drawLine(x4,y4,x2,y2,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line6"] == 1 && result["Line14"] == 1 && result["Line10"] == 1)){
+    drawLine(x6,y6,x1,y1,3,color4);
+    drawLine(x1,y1,x3,y3,3,color4);
+    drawLine(x6,y6,x3,y3,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line13"] == 1 && result["Line9"] == 1 && result["Line8"] == 1)){
+    drawLine(x4,y4,x6,y6,3,color4);
+    drawLine(x2,y2,x6,y6,3,color4);
+    drawLine(x4,y4,x2,y2,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line11"] == 1 && result["Line4"] == 1 && result["Line3"] == 1)){
+    drawLine(x3,y3,x5,y5,3,color4);
+    drawLine(x4,y4,x5,y5,3,color4);
+    drawLine(x3,y3,x4,y4,3,color4);
+    alert_gameOver();
+  }
+  else if((result["Line13"] == 1 && result["Line3"] == 1 && result["Line10"] == 1)){
+    drawLine(x4,y4,x6,y6,3,color4);
+    drawLine(x3,y3,x4,y4,3,color4);
+    drawLine(x6,y6,x3,y3,3,color4);
+    alert_gameOver();
+  }
 }
+function avoid(x,y){
+  if((x>canvasWidth/2.04081633 && x<canvasWidth/1.95121951)&&(y>canvasHeight/4.21052635&&y<canvasHeight/3.80952381)){
+    return 0;
+  }
+  else if ((x>canvasWidth/3.63636364 && x<canvasWidth/3.33333333)&&(y>canvasHeight/2.05128205&&y<canvasHeight/1.96078431)) {
+    return 0;
+  }
+  else if ((x>canvasWidth/3.63636364 && x<canvasWidth/3.33333333)&&(y>canvasHeight/2.75862069&&y<canvasHeight/2.58064516)) {
+    return 0;
+  }
+  else if ((x>canvasWidth/2.66666667 && x<canvasWidth/2.5)&&(y>canvasHeight/3.33333333&&y<canvasHeight/3.07692308)) {
+    return 0;
+  }
+  else if ((x>canvasWidth/1.70212766 && x<canvasWidth/1.63265306)&&(y>canvasHeight/3.33333333&&y<canvasHeight/3.07692308)) {
+    return 0;
+  }
+  else if ((x>canvasWidth/1.42857143 && x<canvasWidth/1.37931034)&&(y>canvasHeight/2.77777778&&y<canvasHeight/2.5974026)) {
+    return 0;
+  }
+  else if ((x>canvasWidth/1.42857143 && x<canvasWidth/1.37931034)&&(y>canvasHeight/2.05128205&&y<canvasHeight/1.96078431)) {
+    return 0;
+ }
+  else if ((x>canvasWidth/1.42857143 && x<canvasWidth/1.37931034)&&(y>canvasHeight/1.63265306&&y<canvasHeight/1.56862745)) {
+    return 0;
+  }
+  else if ((x>canvasWidth/1.66666667 && x<canvasWidth/1.6)&&(y>canvasHeight/1.48148148 &&y<canvasHeight/1.42857143)) {
+    return 0;
+  }
+  else if ((x>canvasWidth/2.05128205 && x<canvasWidth/1.95121951)&&(y>canvasHeight/1.3559322&&y<canvasHeight/1.31147541)) {
+    return 0;
+  }
+  else if ((x>canvasWidth/2.66666667 && x<canvasWidth/2.5)&&(y>canvasHeight/1.48148148 && y<canvasHeight/1.42857143)) {
+    return 0;
+  }
+  else if ((x>canvasWidth/3.63636364 && x<canvasWidth/3.33333333)&&(y>canvasHeight/1.63265306&&y<canvasHeight/1.56862745)) {
+    return 0;
+  }
 
+ else if ((x>canvasWidth/2.05128205 && x<canvasWidth<1.95121951)&&(y>canvasHeight/2.05128205 && y<canvasHeight/1.95121951))
+  {
+    return 0;
+  }
+
+  return 1;
+}
 function click(e){
   mouseX=parseInt(e.clientX-offsetX);
   mouseY=parseInt(e.clientY-offsetY);
-  if((mouseX != x5/4 && mouseY != y1)&&(mouseX != x2 && mouseY != y1/1.16666667)&&(mouseX != x2/1.0502513&& mouseY != y1/1.15702479)&&
-(mouseX != x3/1.85185185 && mouseY != y3/1.83486239)&&(mouseX != x3/1.83486239 && mouseY != y3/1.81818182)&&(mouseX != x3/1.81818182 && mouseY != y3/1.18343195)&&
-(mouseX != x3/1.78571429 && mouseY != y3/1.17647059)&&(mouseX != x3/1.43884892 && mouseY != y1/1.41414141)&&(mouseX != y1 && mouseY != y1/1.4)&&
-(mouseX != y1/1.41843972 && mouseY != y1/1.38613861)&&(mouseX != x5/1.17647059 && mouseY != y1/1.26126126)&&(mouseX != x5/1.18343195 && mouseY != y1/1.26126126)&&
-(mouseX != x5/1.16959064 && mouseY != y1/1.28440367)&&(mouseX != x5&& mouseY != y1/1.17647059)&&(mouseX != x5/1.00502513 && mouseY != y1/1.16666667)&&
-(mouseX != x4/1.29353234 && mouseY != y1/1.15702479)&&(mouseX != x2/1.02564103 && mouseY != y2/1.44927536)&&(mouseX != x2/1.01265823 && mouseY != y2/1.43884892)){
+
+if(avoid(mouseX,mouseY)){
     if(result['Line1'] == 0){
       drawLineAsRect(lineRect1,color2);
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x1,y1,x2,y2,3,color3);
               result["Line1"]= 1;
-              gameOver();
+              gameOver('Line1');
       }
     }
     if(result['Line2'] == 0){
@@ -145,7 +284,7 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x2,y2,x3,y3,3,color3);
               result["Line2"]= 1;
-              gameOver();
+              gameOver('Line2');
       }
     }
     if(result['Line3'] == 0){
@@ -153,7 +292,7 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x3,y3,x4,y4,3,color3);
               result["Line3"]= 1;
-              gameOver();
+              gameOver('Line3');
       }
     }
     if(result['Line4'] == 0){
@@ -161,7 +300,7 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x4,y4,x5,y5,3,color3);
               result["Line4"]= 1;
-              gameOver();
+              gameOver('Line4');
       }
     }
     if(result['Line5'] == 0){
@@ -169,15 +308,15 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x5,y5,x6,y6,3,color3);
               result["Line5"]= 1;
-              gameOver();
+              gameOver('Line5');
       }
     }
     if(result['Line6'] == 0){
       drawLineAsRect(lineRect6,color2);
       if(ctx.isPointInPath(mouseX,mouseY)){
-              drawLine(x6,x6,x1,y1,3,color3);
+              drawLine(x6,y6,x1,y1,3,color3);
               result["Line6"]= 1;
-              gameOver();
+              gameOver('Line6');
       }
     }
     if(result['Line7'] == 0){
@@ -185,7 +324,7 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x1,y1,x4,y4,3,color3);
               result["Line7"]= 1;
-              gameOver();
+              gameOver('Line7');
       }
     }
     if(result['Line8'] == 0){
@@ -193,7 +332,7 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x4,y4,x2,y2,3,color3);
               result["Line8"]= 1;
-              gameOver();
+              gameOver('Line8');
       }
     }
     if(result['Line9'] == 0){
@@ -201,7 +340,7 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x2,y2,x6,y6,3,color3);
               result["Line9"]= 1;
-              gameOver();
+              gameOver('Line9');
       }
     }
     if(result['Line10'] == 0){
@@ -209,7 +348,7 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x6,y6,x3,y3,3,color3);
               result["Line10"]= 1;
-              gameOver();
+              gameOver('Line10');
       }
     }
     if(result['Line11'] == 0){
@@ -217,7 +356,7 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x3,y3,x5,y5,3,color3);
               result["Line11"]= 1;
-              gameOver();
+              gameOver('Line11');
       }
     }
     if(result['Line12'] == 0){
@@ -225,7 +364,7 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x5,y5,x2,y2,3,color3);
               result["Line12"]= 1;
-              gameOver();
+              gameOver('Line12');
       }
     }
     if(result['Line13'] == 0){
@@ -233,7 +372,7 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x4,y4,x6,y6,3,color3);
               result["Line13"]= 1;
-              gameOver();
+              gameOver('Line13');
       }
     }
     if(result['Line14'] == 0){
@@ -241,7 +380,7 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x1,y1,x3,y3,3,color3);
               result["Line14"]= 1;
-              gameOver();
+              gameOver('Line14');
       }
     }
     if(result['Line15'] == 0){
@@ -249,11 +388,11 @@ function click(e){
       if(ctx.isPointInPath(mouseX,mouseY)){
               drawLine(x5,y5,x1,y1,3,color3);
               result["Line15"]= 1;
-              gameOver();
+              gameOver('Line15');
       }
     }
   }
-
+//  console.log("x = "+mouseX+"y = "+mouseY);
 }
 
 canvas.addEventListener("click", click);
